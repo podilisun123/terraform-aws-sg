7fc37c0eb0f14b4f22239b5967868b2ba0e523ec
sundar <sudar@gmail.com>
2025-10-30 14:25:12 +0530

    security group

--- main.tf
+++ main.tf
@@ -6,23 +6,19 @@ resource "aws_security_group" "allow_tls" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-        [
             from_port = ingress.value["from_port"]
             to_port = ingress.value["to_port"]
             protocol = ingress.value["protocol"]
             cidr_blocks = ingress.value[cidr_blocks]
-        ]
     }
   }
   dynamic "egress" {
     for_each = var.egress_rules
     content {
-        [
             from_port = engress.value["from_port"]
             to_port = egress.value["to_port"]
             protocol = egress.value["protocol"]
             cidr_blocks = egress.value[cidr_blocks]
-        ]
     }
   }
   tags = merge(
